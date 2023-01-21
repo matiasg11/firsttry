08a748db421d15429a4c24210dec3aab28cc7b53
Matias Guarnera <matiasguarnera@gmail.com>
2023-01-21 18:35:49 -0300

    Starting the first try

---
+++ src/components/counter.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Aguante Boca</h1>
+                <button>Increment</button>
+            </div>
+        );
+    }
+}
+export default Counter;
