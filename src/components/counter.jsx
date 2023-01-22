a2ced151c473429b9bd869fefa0f420e241ce128
Matias Guarnera <matiasguarnera@gmail.com>
2023-01-22 11:03:58 -0300

    Conditional handling

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -9,7 +9,7 @@ class Counter extends Component {
     render() {
         return (
             <div>
-                {this.state.tags.length === 0 && "Please create a new tag"}
+                {this.state.tags.length === 0 && "Please create a new tag!"}
                 {this.renderTags()}
             </div>
         );
