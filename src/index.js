3dab21a564eeba0faf53e598f030facb77b55d4c
Matias Guarnera <matiasguarnera@gmail.com>
2023-01-22 13:42:00 -0300

    App and fake movies and genres

--- src/index.js
+++ src/index.js
@@ -5,11 +5,12 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import Counter from "./components/counter";
 import "bootstrap/dist/css/bootstrap.css";
+import "font-awesome/css/font-awesome.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <Counter />
+        <App />
     </React.StrictMode>
 );
 
