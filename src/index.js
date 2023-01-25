fc006c117141189a32326f39ff07e28de8ecf4e4
Matias Guarnera <matiasguarnera@gmail.com>
2023-01-24 22:03:37 -0300

    Create a bigger component: Counters

--- src/index.js
+++ src/index.js
@@ -5,15 +5,18 @@ import App from "./App";
 import Movies from "./components/movies";
 import reportWebVitals from "./reportWebVitals";
 import Counter from "./components/counter";
+import Counters from "./components/counters.jsx";
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <Movies />
+        <Counters />
     </React.StrictMode>
 );
+
+// ReactDOM.render(<Movies />, document.getElementById("root")); Old way to do it
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
