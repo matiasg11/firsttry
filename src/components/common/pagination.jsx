5944d00fa489ff12d779cc8e106058391e021e40
Matias Guarnera <matiasguarnera@gmail.com>
2023-01-31 22:30:26 -0300

    Refactoring code to remove props from Stateless functional components

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -2,9 +2,8 @@ import React from "react";
 import _ from "lodash";
 import propTypes from "prop-types";
 
-const Pagination = (props) => {
-    const { itemsCount, pageSize, currentPage, onPageChange } = props;
-    console.log(currentPage);
+const Pagination = ({ itemsCount, pageSize, currentPage, onPageChange }) => {
+    // console.log(currentPage);
 
     const pagesCount = Math.ceil(itemsCount / pageSize);
     if (pagesCount == 1) {
