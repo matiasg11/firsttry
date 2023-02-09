3e08bf25a87465f7fa0b5eef120f2a721a014cda
Matias Guarnera <matiasguarnera@gmail.com>
2023-02-09 20:37:04 -0300

    Index.js corrected

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowseRouter, BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
-import Movies from "./components/movies";
 import reportWebVitals from "./reportWebVitals";
-import Counter from "./components/counter";
-import Counters from "./components/counters.jsx";
-import NavBar from "./components/navbar";
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
 
