6f1e2aaeebd2d62d0a7d06e81526805bbbde8611
Matias Guarnera <matiasguarnera@gmail.com>
2023-02-23 13:35:33 -0300

    Starting with the homework. LoginForm

--- src/components/movieForm.jsx
+++ src/components/movieForm.jsx
@@ -1,16 +1,63 @@
-import React from "react";
+import React, { Component } from "react";
+import Joi from "joi-browser";
+import Form from "./common/form";
 
-const MovieForm = ({ match, history }) => {
-    return (
-        <div>
-            <h1>Movie Form {match.params.id}</h1>
-            <button
-                className="btn btn-primary"
-                onClick={() => history.push("/movies")}>
-                Save
-            </button>
-        </div>
-    );
-};
+class MovieForm extends Form {
+    state = {
+        data: { username: "", password: "" },
+        errors: {},
+    };
+
+    schema = {
+        title: Joi.string().required().label("Title"),
+        genre: Joi.string().required().label("Genre"),
+        numberInStock: Joi.string()
+            .required()
+            .min(0)
+            .max(99)
+            .label("Number in Stock"),
+        dailyRentalRate: Joi.string().required().min(0).max(10).label("Rate"),
+    };
+
+    doSubmit = () => {
+        //Call the server and then redirect the user to another place
+        console.log("Submitted");
+    };
+
+    render() {
+        return (
+            <div>
+                <h1>Movie Form</h1>
+                <form onSubmit={this.handleSubmit}>
+                    {this.renderInput("title", "Title")}
+                    {this.renderInput("genre", "Genre")}
+                    {this.renderInput(
+                        "numberInStock",
+                        "Number in Stock",
+                        "number"
+                    )}
+                    {this.renderInput("dailyRentalRate", "Rate", "number")}
+                    {this.renderButton("Save")}
+                </form>
+            </div>
+        );
+    }
+}
 
 export default MovieForm;
+
+// const MovieForm = ({ match, history }) => {
+//     return (
+// <div>
+//     <h1>Movie Form {match.params.id}</h1>
+//     <Form />
+//     <button
+//         className="btn btn-primary"
+//         onClick={() => history.push("/movies")}>
+//         Save
+//     </button>
+// </div>
+//     );
+// };
+
+// export default MovieForm;
